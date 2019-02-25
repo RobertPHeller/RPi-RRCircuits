9fa431a7243476d8c286b3f6b427bd48525ce637
Robert Heller <heller@deepsoft.com>
2019-02-25 12:10:29 -0500

    Fix config.mk (make a local copy!).

--- ovalsidingControl_nucleo_io/config.mk
+++ ovalsidingControl_nucleo_io/config.mk
@@ -1,1 +1,11 @@
-/home/heller/openmrn/applications/default_config.mk
+ifndef APP_PATH
+APP_PATH := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+endif
+export APP_PATH
+
+-include $(APP_PATH)/openmrnpath.mk
+ifndef OPENMRNPATH
+OPENMRNPATH := $(realpath $(APP_PATH)/../..)
+endif
+export OPENMRNPATH
+GITREPOS += /home/heller/RRCircuits
