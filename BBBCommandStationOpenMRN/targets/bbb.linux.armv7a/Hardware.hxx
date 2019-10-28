4f5e17e1be167242fbbb4d90314d8f55ea064e4c
Robert Heller <heller@deepsoft.com>
2019-10-28 13:35:45 +0000

    Add GPIO for control and status GPIO.  Also start classes for HBridge and Fan Status and Control.

--- BBBCommandStationOpenMRN/targets/bbb.linux.armv7a/Hardware.hxx
+++ BBBCommandStationOpenMRN/targets/bbb.linux.armv7a/Hardware.hxx
@@ -6,6 +6,12 @@
 
 #define HARDWARE_IMPL "BBB Command Station"
 
+#define FANPin GpioOutputSafeLow
+#define EnablePin GpioOutputSafeHigh
+#define ThermFlagPin GpioInputActiveLow
+#define CurrentFromAIN(val) ((val)*.0007326007)
+#define TempFromAIN(val) ((((val)*.0004395604)/.01)+(-50))
+
 // On chip GPIO:
 //
 // Mains:
@@ -13,11 +19,13 @@
 // DCC P8_12 (PRU 0, R30.14) [config-pin P8_12 pruout]
 // EN  P8-10 GPIO_68 (2.04)  [config-pin P8_10 gpio]
 // CSense P9-39 AIN0         [/sys/bus/iio/devices/iio:device0/in_voltage0_raw]
+// TF  P8-11 GPIO_45 (1.13)  [config-pin P8_11 gpio_input]
 
 #define CSenseMainAnalogChannel 0
-#define CurrentFromAIN(val) ((val)*.0007326007)
+GPIO_PIN(MainEN, EnablePin, 68);
+GPIO_PIN(MainTF, ThermFlagPin, 45);
 
-// TF  P8-11 GPIO_45 (1.13)  [config-pin P8_11 gpio_input]
+
 //
 // Prog Track:
 //
@@ -28,22 +36,24 @@
 //
 
 #define CSenseProgAnalogChannel 1
-
+GPIO_PIN(ProgEN, EnablePin, 46);
+GPIO_PIN(ProgTF, ThermFlagPin, 65);
 // Tempsensor:
 //
 // P9-37 AIN2                [/sys/bus/iio/devices/iio:device0/in_voltage2_raw]
 //
 
 #define TempsensorChannel 2
-#define TempFromAIN(val) ((((val)*.0004395604)/.01)+(-50))
 
 // Fan Control (out):
 //
 // P8-14 GPIO_26 (0.26)      [config-pin P8_14 gpio]
 //
 
+GPIO(FanControl, FANPin, 26);
 
-typedef GpioInitializer<> GpioInit;
+typedef GpioInitializer<MainEN_Pin, MainTF_Pin, ProgEN_Pin, 
+                        ProgTF_Pin, FanControl_Pin> GpioInit;
 
 //#define USE_OPENLCB_TCP_HOST
 //#define DEFAULT_OPENLCB_TCP_HOST "localhost"
