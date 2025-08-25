b417222c01abc00e9d46f83a40d559087c1ec70d
Robert Heller <heller@deepsoft.com>
2025-08-25 07:53:50 -0400

    Add configpins.sh script.

---
+++ IO_CAPE/configpins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+/usr/bin/config-pin P9.12 gpio_pu
+/usr/bin/config-pin P9.14 gpio_pu
+/usr/bin/config-pin P9.15 gpio_pu
+/usr/bin/config-pin P9.16 gpio_pu
+/usr/bin/config-pin P9.23 gpio
+/usr/bin/config-pin P9.25 gpio
+/usr/bin/config-pin P9.27 gpio
+/usr/bin/config-pin P9.30 gpio
+/usr/bin/config-pin P8.07 gpio_pu
+/usr/bin/config-pin P8.08 gpio_pu
+/usr/bin/config-pin P8.09 gpio_pu
+/usr/bin/config-pin P8.10 gpio_pu
+/usr/bin/config-pin P8.11 gpio
+/usr/bin/config-pin P8.12 gpio
+/usr/bin/config-pin P8.15 gpio
+/usr/bin/config-pin P8.16 gpio
